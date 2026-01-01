c57f0635f365da17875cf9d58725c382eb53123f
Prakriti Patel <167187253+Prakriti-18@users.noreply.github.com>
2026-01-01 19:50:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function insert_Row() {
     //Write your code here
-  
+  const table = document.getElementById("sampleTable");
+
+  // insert new row at index 0 (top)
+  const newRow = table.insertRow(0);
+
+  // insert cells
+  const cell1 = newRow.insertCell(0);
+  const cell2 = newRow.insertCell(1);
+
+  // set cell values
+  cell1.innerHTML = "New Cell1";
+  cell2.innerHTML = "New Cell2";
   
 }
